23aa84820f7ace8c9cb94c833621b142f5df54ac
Chandra, Rajdeep <rajdeep.chandra@accenture.com>
2019-04-05 13:51:57 +0530

    change

--- src/SearchGify.js
+++ src/SearchGify.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 export const SearchGify = () => {
 
-    const [giphyArray, setgiphyArray] = useState([]);
+   // const [giphyArray, setgiphyArray] = useState([]);
     const [inputValue, setinputValue] = useState('');
+    const [gifValue, setgifValue] = useState('');
+    let giphyArray = [];
+    let i = 0;
 
-
-    const apiKey = '';
+    const apiKey = '1xGPgNOHn0R3iRpJtMlxj0hooj5gubLQ';
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`
     const objParam = {
         limit: '25',
@@ -29,14 +31,33 @@ export const SearchGify = () => {
         setinputValue(e.target.value)
     }
 
+    const changeGif = () => {
+        // for(let i = 0;i<giphyArray.length;i++){
+        //     setTimeout(() => {
+        //         console.log("index" + giphyArray[i].embed_url)
+        //         setgifValue(giphyArray[i].embed_url);
+        //     },2000)
+        // }
+        if(i < giphyArray.length) {
+            setTimeout(() => {
+                console.log("index" + giphyArray[i].embed_url)
+                setgifValue(giphyArray[i].embed_url);
+                i++;
+                changeGif();
+            }, 1000)
+        }
+
+    }
+
 
     const handleClick = () => {
+        i = 0;
         console.log("value from input" + inputValue)
         axios.get(serializeGetParams(inputValue))
             .then(response => {
-                console.log(JSON.stringify(response.data.data));
-                setgiphyArray(response.data.data);
-
+                console.log("response from api" + JSON.stringify(response.data.data));
+                giphyArray = response.data.data;
+                changeGif();
             })
             .catch(err => {
                 console.log("Error" + err);
@@ -49,14 +70,10 @@ export const SearchGify = () => {
             <button className="btn btn-block" onClick={handleClick}>Submit</button>
 
             <div className="gifyContainer">
-                {giphyArray.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            <img src={item.embed_url} alt=""/>
-
-                        </div>
-                    )
-                })}
+            <div>
+                <iframe src={gifValue}></iframe>
+            </div>
+        
 
             </div>
         </div>
